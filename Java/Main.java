1a25539189c4872f0d333de9f11d5b91f5e5f414
tblendinger <165098917+tblendinger@users.noreply.github.com>
2025-10-15 20:14:09 -0300

    Update Main.java

--- Java/Main.java
+++ Java/Main.java
@@ -1,15 +1,7 @@
-<<<<<<< HEAD
 package Java;
 
 public class Main {  
     public static void main(String[] args) {
         System.out.println("Hola Mundo");
     }
-    
-=======
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hola Mundo");
-    }
->>>>>>> d53ccf1cd944520f6bd40e35073c48c8070ebc37
 }
